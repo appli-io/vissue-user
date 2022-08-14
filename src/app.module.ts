e1bfd49bf401e9915a1ff4e2d43801bb05d017fc
David Misael Villegas Sandoval <david.misa97@gmail.com>
2022-08-14 05:20:38 -0400

    [Config]
    -> Initial commit and configurations to install typeorm, pg, etc.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module }        from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService }    from './app.service';
+import { User }          from './module/user/entity/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule }    from './module/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'user-pass',
+      database: 'ms-user',
+      synchronize: true,
+      entities: [User]
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
